0018d7ca6ce20832e54678b2f951b5252bbd9ac5
Colin de Groot <colindegroot2004@gmail.com>
2021-04-12 11:33:04 +0200

    kleine bug verwijderd +styling

---
+++ lossebestanden/extradarkode.js
@@ -0,0 +1,13 @@
+var toggleDarkmode = document.querySelector(".toggle-btn input");
+
+// console.log(toggleDarkmode);
+
+toggleDarkmode.addEventListener("click", toggleDarkmodeAppearance);
+
+function toggleDarkmodeAppearance() {
+  if (toggleDarkmode.checked == true) {
+    document.querySelector("body").classList.add("darkmode");
+  } else {
+    document.querySelector("body").classList.remove("darkmode");
+  }
+}
